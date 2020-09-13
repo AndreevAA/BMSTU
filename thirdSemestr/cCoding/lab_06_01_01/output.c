e00dc84089afb49b6d70694047db3c23b0cf4e6b
Andreev Alexander <andreev.work.mail@gmail.com>
2020-09-13 15:52:25 +0300

    Вывод даты в инт

--- thirdSemestr/cCoding/lab_06_01_01/output.c
+++ thirdSemestr/cCoding/lab_06_01_01/output.c
@@ -6,11 +6,12 @@
 
 void output_data(char * title, char * name, char * year)
 {
-	printf("%s\n%s\n%s\n", title, name, year);
+	printf("%s\n%s\n%d\n", title, name, atoi(year));
 }
 
 void output_sorted_data(struct films* all_films, int number_of_films)
 {
 	for (int i = 0; i < number_of_films; i++)
-		output_data((all_films)[i].title, (all_films)[i].name, (all_films)[i].year);
+		if (strlen((all_films)[i].title) > 0)
+			output_data((all_films)[i].title, (all_films)[i].name, (all_films)[i].year);
 }
