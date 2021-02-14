93bc7d80e8cfdccb5927cd88ca6a0775a4151abc
Andreev Alexander <andreev.work.mail@gmail.com>
2021-02-14 22:41:06 +0300

    Работающий код для Ньютона

--- fourthSemestr/computationalAlgorithms/LR_1/src/main.c
+++ fourthSemestr/computationalAlgorithms/LR_1/src/main.c
@@ -1,37 +1,14 @@
 #include "../inc/config.h"
 #include "../inc/io.h"
-
-void out_programm_end()
-{
-	printf("Programm has ended work...\n");
-}
-
-void welcome_command()
-{
-	printf("Construction and software implementation of an algorithm for polynomial interpolation of table functions.\n\n");
-}
-
-double get_function_value_with_polynom_level_newton(int polynom_level, point_t **points, int number_of_points)
-{
-	return 1.0;
-}
-
-double get_function_value_with_polynom_level_ermitta(int polynom_level, point_t **points, int number_of_points)
-{
-	return 1.0;
-}
-
-void out_table_head()
-{
-	printf("n / y(x) | P.Newton | P.Ermitta\n");
-}
+#include "../inc/polynom_newton.h"
+#include "../inc/polynom_ermit.h"
 
 void get_comparation_table(int min_polynom_level, int max_polynom_level, point_t **points, int number_of_points)
 {
 	out_table_head();
 
 	for (int temp_polynom_level = min_polynom_level; temp_polynom_level <= max_polynom_level; temp_polynom_level++)
-		printf("%d | %lf | %lf\n", temp_polynom_level, get_function_value_with_polynom_level_newton(temp_polynom_level, points, number_of_points), get_function_value_with_polynom_level_ermitta(temp_polynom_level, points, number_of_points));
+		printf("%d | %lf | %lf\n", temp_polynom_level, get_function_value_with_polynom_level_newton(temp_polynom_level, points, number_of_points), get_function_value_with_polynom_level_ermit(temp_polynom_level, points, number_of_points));
 }
 
 void get_sqrt_with_interpolation_with_newton(point_t **points, int number_of_points)
