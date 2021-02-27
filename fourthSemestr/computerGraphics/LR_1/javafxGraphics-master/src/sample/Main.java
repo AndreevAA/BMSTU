b67f4aa15bd54f3aabe78128fc252054c1a0ab32
Andreev Alexander <andreev.work.mail@gmail.com>
2021-02-27 22:14:30 +0300

    Добавление выполенной, защищенной и сданной ЛР1

--- fourthSemestr/computerGraphics/LR_1/javafxGraphics-master/src/sample/Main.java
+++ fourthSemestr/computerGraphics/LR_1/javafxGraphics-master/src/sample/Main.java
@@ -5,23 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.SceneAntialiasing;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.LineTo;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception
-    {
+    public void start(Stage stage) throws Exception {
 
         Group group = new Group();
         group.autoSizeChildrenProperty().setValue(true);
@@ -31,7 +21,7 @@ public class Main extends Application {
         Parent content = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene scene = new Scene(content, -1, -1);
 
-        stage.setTitle("computerGraphics, LR_1, Andreev A.A.");
+        stage.setTitle("LR_1, Andreev A.A.");
         stage.setScene(scene);
         stage.sizeToScene();
         stage.setResizable(true);
