e0be8ab220bd565560025399415f55b46b9308b8
AndreevAA <58621780+AndreevAA@users.noreply.github.com>
2021-03-11 22:51:13 +0300

    Обновлние

--- fourthSemestr/computationalAlgorithms/LR_2/interpolation.py
+++ fourthSemestr/computationalAlgorithms/LR_2/interpolation.py
@@ -14,25 +14,23 @@ def getNearNumbers(tempInputList, x):
         else: leftCur = int((leftCur + rightCur) / 2) + 1
     return rightCur
 
+# Рекурсивна обработка до единичного
+def getCongestionValue(xValues, yValues):
+    numberOfColls = len(xValues)
+    while numberOfColls != 1:
+    	return (getCongestionValue(xValues[:-1], yValues[:-1]) - getCongestionValue(xValues[1:], yValues[1:])) / (xValues[0] - xValues[numberOfColls - 1]);
+    return yValues[0];
 
-def recurs_function(xs, ys):
-    l = len(xs)
-    if l == 1:
-        return ys[0]
-    else:
-        return (recurs_function(xs[:-1], ys[:-1]) - recurs_function(xs[1:], ys[1:])) / (xs[0] - xs[l - 1])
-
-
+# Получение значения интерполяции Ньютона 
 def getNumberByNewtonInterpolationInSamples(tempInputListX, tempInputListZ, x):
-    z_x = tempInputListZ[0]
-    for i in range(1, len(tempInputListX)):
-        k = 1
-        for j in range(i):
-            k *= (x - tempInputListX[j])
-        dd = recurs_function(tempInputListX[:i+1], tempInputListZ[:i+1])
-        z_x += (k * recurs_function(tempInputListX[:i+1], tempInputListZ[:i+1]))
-    return z_x
+    answerNumberByNewtonInterpolationInSamples = tempInputListZ[0]
+    for rightCur in range(1, len(tempInputListX)):
+        multiplyedValue = 1
+        for leftCur in range(rightCur): multiplyedValue *= (x - tempInputListX[leftCur])
+        answerNumberByNewtonInterpolationInSamples += (multiplyedValue * getCongestionValue(tempInputListX[:rightCur + 1], tempInputListZ[:rightCur + 1]))
+    return answerNumberByNewtonInterpolationInSamples
 
+# Преобразование таблицы в удобный вид
 def getTrasmittedTable(dataCompRow, data):
 	try:
 		tempZline = [];
@@ -49,12 +47,15 @@ def getTrasmittedTable(dataCompRow, data):
 	except:
 		return globals.ERROR_STATUS;
 
+# Получение среднего для старта
 def getMiddleCellStart(nearestList, nLevel):
 	return nearestList - int(ceil((nLevel + 1) / 2));
 
+# Получение среднего для конца
 def getMiddleCellEnd(nearestList, nLevel):
 	return nearestList + int(ceil((nLevel + 1) / 2));
 
+# Установка всех промежутков для каждого этапа по Х, У, Z
 def setAllSamples(allSamples, dataCompRow, dataSize, x, y, nx, ny):
 	i_y = getNearNumbers(dataCompRow[1], y) 
 
@@ -68,37 +69,34 @@ def setAllSamples(allSamples, dataCompRow, dataSize, x, y, nx, ny):
 		else:
 			allSamples[1], allSamples[2] = dataCompRow[1][getMiddleCellStart(i_y, ny) - 1: getMiddleCellEnd(i_y, ny)], dataCompRow[2][getMiddleCellStart(i_y, ny) - 1: getMiddleCellEnd(i_y, ny)]
 
+# Сдвиг курсоров 
 def setAllCurs(allSamples, dataCompRow, dataSize, x, y, nx, ny):
-	left = 0
-	right = 0	
-
-	i_x = getNearNumbers(dataCompRow[0], x)
-
-	if i_x - (nx + 1) / 2 < 0:
-		allSamples[0] = dataCompRow[0][:int(getMiddleCellEnd(i_x, nx) + 1)]
-		right = getMiddleCellEnd(i_x, nx) + 1
-	elif dataSize < i_x + (nx + 1) / 2:
-		allSamples[0] = dataCompRow[0][getMiddleCellStart(i_x, nx):]
-		left = getMiddleCellStart(i_x, nx)
-		right = 6
+	leftCur, rightCur = 0, 0;
+	nearestXValueNumber = getNearNumbers(dataCompRow[0], x)
+	if nearestXValueNumber - (nx + 1) / 2 < 0:
+		allSamples[0] = dataCompRow[0][:int(getMiddleCellEnd(nearestXValueNumber, nx) + 1)]
+		right = getMiddleCellEnd(nearestXValueNumber, nx) + 1
+	elif dataSize < nearestXValueNumber + (nx + 1) / 2:
+		allSamples[0] = dataCompRow[0][getMiddleCellStart(nearestXValueNumber, nx):]
+		leftCur, rightCur = getMiddleCellStart(nearestXValueNumber, nx), 6
 	elif nx % 2 != 0:
-		allSamples[0] = dataCompRow[0][getMiddleCellStart(i_x, nx): getMiddleCellEnd(i_x, nx)]
-		left = getMiddleCellStart(i_x, nx)
-		right = getMiddleCellEnd(i_x, nx)
+		allSamples[0] = dataCompRow[0][getMiddleCellStart(nearestXValueNumber, nx): getMiddleCellEnd(nearestXValueNumber, nx)]
+		leftCur, rightCur = getMiddleCellStart(nearestXValueNumber, nx), getMiddleCellEnd(nearestXValueNumber, nx)
 	else:
-		allSamples[0] = dataCompRow[0][getMiddleCellStart(i_x, nx) - 1: getMiddleCellEnd(i_x, nx)]
-		left = getMiddleCellStart(i_x, nx) - 1
-		right = getMiddleCellEnd(i_x, nx)
+		allSamples[0] = dataCompRow[0][getMiddleCellStart(nearestXValueNumber, nx) - 1: getMiddleCellEnd(nearestXValueNumber, nx)]
+		leftCur, rightCur = getMiddleCellStart(nearestXValueNumber, nx) - 1, getMiddleCellEnd(nearestXValueNumber, nx)
 
-	for i in range(len(allSamples[2])):
-		allSamples[2][i] = allSamples[2][i][int(left):int(right)]
+	for tempCur in range(len(allSamples[2])):
+		allSamples[2][tempCur] = allSamples[2][tempCur][int(lefleftCurt):int(rightCur)]
 
+# Заполнение массива значениями интерполяции
 def getAnswerListOfNewtonInterpolations(allSamples, x):
 	aswerListOfNewtonInterpolations = list();
 	for tempCur in range(len(allSamples[1])):
 		aswerListOfNewtonInterpolations.append(getNumberByNewtonInterpolationInSamples(allSamples[0], allSamples[2][tempCur], x))
 	return aswerListOfNewtonInterpolations;
 
+# Основной компаратор вызова функций обработки
 def getComparatorStatus(dataCompRow, data, x, y, nx, ny, functionValue, newtonInterpolationValue):
 	try:
 	    allSamples = [[], [], []]
