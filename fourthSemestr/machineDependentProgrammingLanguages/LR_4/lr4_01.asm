388db1665f46a598ef8774f948a0a7035db73131
Andreev Alexander <andreev.work.mail@gmail.com>
2021-03-20 07:19:51 +0300

    Update

--- fourthSemestr/machineDependentProgrammingLanguages/LR_4/lr4_01.asm
+++ fourthSemestr/machineDependentProgrammingLanguages/LR_4/lr4_01.asm
@@ -4,32 +4,44 @@ StkSeg ENDS
 ;
 ;---------------------
 DATASG SEGMENT PARA
-	    MAX_LEN = 10
-	    STRING DB MAX_LEN, MAX_LEN DUP('$')
+	    MAX_LEN = 10 		; установил максимальный размер введенной строки
+	    STRING DB MAX_LEN, MAX_LEN DUP('$') ; Передал строке максимальный размер ввода и указал последний символ строки
 DATASG ENDS
 ;---------------------
 Code SEGMENT PARA
    		ASSUME DS:DATASG, CS:Code, ES:Code
-START:
-	    .386
-	    PUSH DS
-	    MOV AX, DATASG
-	    MOV DS, AX
-	    MOV ES, AX
-	    XOR AX, AX
-	    ;--------------------
-	    LEA DX, STRING
+;---------------------
+INPUT:
+		LEA DX, STRING
 	    MOV AH, 10
 	    INT 21H
 
-	    ;вывод на экран
-	    MOV STRING+1, 0AH
+	    RET
+;---------------------
+OUTPUT: 
+		MOV STRING+1, 0AH
 	    LEA DX, STRING+1
 	    MOV AH, 9
 	    INT 21H
 
-	    ;завершение программы
-	    MOV AX, 4C00h
-	    INT 21h
+	    RET
+;---------------------	
+START:
+	    .386
+	    PUSH DS
+	    MOV AX, DATASG		; загрузка в AX адреса сегмента данных
+	    MOV DS, AX			; установка DS
+	    MOV ES, AX			; установка ES
+	    XOR AX, AX
+
+	    ;Ввод данных
+	    CALL INPUT
+
+	    ;Вывод на экран
+	    CALL OUTPUT
+
+	    ;Завершение программы
+	    MOV AX, 4C00h		; AH=4Ch завершение процесса
+	    INT 21H				; вызов функции DOS
 Code 	ENDS
 		END START
