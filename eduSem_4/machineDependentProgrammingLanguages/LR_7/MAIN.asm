e4d0bc0ef1badefa5b825eacc48e95fa40eef57b
AndreevAA <58621780+AndreevAA@users.noreply.github.com>
2021-04-18 13:24:18 +0300

    Обновление ЛР7 по МЗЯП на моменты

--- eduSem_4/machineDependentProgrammingLanguages/LR_7/MAIN.asm
+++ eduSem_4/machineDependentProgrammingLanguages/LR_7/MAIN.asm
@@ -1,52 +1,105 @@
-CODES SEGMENT PARA PUBLIC 'CODE'
-         ASSUME CS:CODES, DS:CODES
-ORG 100H
-BEGIN:  JMP INIT
-SAVEINT9 DD ?
-MSG DB "Andreev", 10, 13
-NEWINT9:
-        IN AL, 60H
-        CMP AL, 42
-        JE UNINSTALL
-        CMP AL, 58
-        JNE EXIT
-        MOV AL, 1
-        MOV BH, 0
-        MOV BL, 11110000B
-        MOV CX, 9
-        MOV DH, 24
-        MOV DL, 11
-        PUSH CS
-        POP ES
-        MOV BP, OFFSET MSG
-        MOV AH, 13H
-        INT 10H
-EXIT:
-        JMP CS:SAVEINT9
-UNINSTALL:
-        MOV AX, 2509H
-        LDS DX, CS:SAVEINT9
-        INT 21H
-        MOV ES, CS:2CH
-        MOV AH, 49H
-        INT 21H
-        PUSH CS
-        POP ES
-        MOV AH, 49H
-        INT 21H
-        JMP CS:SAVEINT9
-INIT:
-        CLI
-        MOV AX, 3509H
-        INT 21H
-        MOV WORD PTR SAVEINT9, BX
-        MOV WORD PTR SAVEINT9+2,ES
-        MOV AX, 2509H
-        MOV DX, OFFSET NEWINT9
-        INT 21H
-        MOV AX, 3100H
-        MOV DX, (init - begin + 10FH) / 16
-        STI
-        INT 21H
+.MODEL TINY
+.186
+
+CODES SEGMENT
+    ASSUME CS:CODES, DS:CODES
+    ORG 100H
+
+MAIN:
+    jmp init
+    cur db 0
+    speed db 01fh
+    OLD_8H dd ?
+    FLAG db 228
+
+MY_NEW_8H proc
+    pusha
+    pushf
+    push es
+    push ds
+
+    mov ah, 02h
+    int 1ah
+
+    cmp dh, cur
+    mov cur, dh
+    je end_loop
+
+    mov al, 0F3h
+    out 60h, al
+    mov al, speed
+    out 60h, al
+
+    dec speed
+    test speed, 01fh
+    jz reset
+    jmp end_loop
+
+    reset:
+        mov speed, 01fh
+
+    end_loop:
+        pop ds
+        pop es
+
+        popf
+        popa
+
+        jmp CS:OLD_8H
+
+MY_NEW_8H endp
+
+init:
+    mov AX, 3508h
+    int 21h
+
+    cmp ES:FLAG, 228
+    je uninstall
+
+    mov word ptr OLD_8H, BX
+    mov word ptr OLD_8H + 2, ES
+
+    mov AX, 2508h
+
+    mov DX, offset MY_NEW_8H
+    int 21h
+
+    mov DX, offset INSTALL_MSG
+    mov AH, 9
+    int 21h
+
+    mov DX, offset init
+    int 27H
+
+uninstall:
+    push ES
+    push DS
+
+    mov al, 0F3h
+    out 60h, al
+    mov al, 0
+    out 60h, al
+
+    mov DX, word ptr ES:OLD_8H
+    MOV DS, word ptr ES:OLD_8H + 2
+    mov AX, 2508h
+    int 21h
+
+    pop DS
+    pop ES
+
+    mov AH, 49h
+    int 21h
+
+    mov DX, offset UNINSTALL_MSG
+    mov AH, 9h
+    int 21h
+
+    mov AX, 4C00h
+    int 21h
+
+    INSTALL_MSG   DB 'Install custom breaking$'
+    UNINSTALL_MSG DB 'Uninstall custom breaking$'
+
 CODES ENDS
-END BEGIN
+END MAIN
