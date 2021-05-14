d6b6f9227d5dfde6d04689142345524c26ce9abc
Andreev Alexander <andreev.work.mail@gmail.com>
2021-05-14 15:42:52 +0300

    Модификация отрисовки

--- eduSem_4/computerGraphics/LR_7/src/sample/Controller.java
+++ eduSem_4/computerGraphics/LR_7/src/sample/Controller.java
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static sample.graphical.entity.CanvasOperations.*;
+import static sample.graphical.entity.Figure.*;
 
 public class Controller implements Initializable {
 
@@ -33,6 +34,8 @@ public class Controller implements Initializable {
 
     List<List<GraphicalPoint>> allFigures;
 
+    List<ColoredPixel> pixelList;
+
     RectangleCutter mainRectangle;
 
     @FXML
@@ -42,10 +45,13 @@ public class Controller implements Initializable {
     private Canvas graphTable;
 
     @FXML
+    private Button addFigureButton;
+
+    @FXML
     private ScrollPane scrollPanel;
 
     @FXML
-    private Button clearAllButton, comeBack;
+    private Button clearAllButton, comeBack, fillButton;
 
     @FXML
     private ChoiceBox timeDelayType = new ChoiceBox(), colorFillingType = new ChoiceBox();
@@ -70,6 +76,9 @@ public class Controller implements Initializable {
         // Массив всех фигур
         allFigures = new ArrayList<>();
 
+        // Массив пикселей
+        pixelList = new ArrayList<>();
+
         // Массив копий
         copyObjectList = new ArrayList<>();
 
@@ -92,7 +101,7 @@ public class Controller implements Initializable {
     @FXML
     public void onComeBack() {
         comeBack.setOnAction(actionEvent -> {
-            objectList = getCopyFunction(objectList, copyObjectList);
+//            objectList = getCopyFunction(objectList, copyObjectList);
             objectsPlacedList.setItems(
                     FXCollections.observableList(objectList.stream().map(Object::toString).collect(Collectors.toList())));
             redrawElements(graphTable, objectList);
@@ -102,11 +111,12 @@ public class Controller implements Initializable {
     @FXML
     public void onClearAllButton() {
         clearAllButton.setOnAction(event -> {
-            copyObjectList = createCopyFunction(objectList, copyObjectList);
+            //copyObjectList = createCopyFunction(objectList, copyObjectList);
 
             objectList.clear();
             tempFigure.clear();
             allFigures.clear();
+            pixelList.clear();
 
             redrawElements(graphTable, objectList);
         });
@@ -138,6 +148,36 @@ public class Controller implements Initializable {
                 tempFigure.clear();
 
             redrawElements(graphTable, objectList);
+        });
+    }
+
+    @FXML
+    public void onAddFigure() {
+
+        // Нажатие на кнопку Замкнуть
+        addFigureButton.setOnAction(actionEvent -> {
+           if (isFigureAvailiableToCreate(tempFigure)) {
+               objectList = addLine(graphTable, objectList, tempFigure.get(0).xValue, tempFigure.get(0).yValue, tempFigure.get(tempFigure.size() - 1).xValue, tempFigure.get(tempFigure.size() - 1).yValue, Color.BLACK);
+               tempFigure = addFigure(allFigures, tempFigure);
+           }
+           else
+               parameterErrorField.setText("Введите 3 и более точек!");
+        });
+    }
+
+    @FXML
+    public void onFill() {
+        fillButton.setOnAction(actionEvent -> {
+            if (isAllFiguresAvailiableToBeFilles(allFigures)) {
+
+                long startTime = System.nanoTime();
+                fillAllFigures(graphTable, allFigures, pixelList, CanvasOperations.getColor(colorFillingType.getValue().toString()));
+                long endTime = System.nanoTime();
+
+                workingTime.setText(Long.toString((endTime - startTime) / 100000000));
+            }
+            else
+                parameterErrorField.setText("Должна быть как минимум одна замкнутая фигура!");
         });
     }
 }
