8546a926ebbcd215f3b9275ca806ccde8c17714e
Andreev Alexander <andreev.work.mail@gmail.com>
2021-05-22 15:39:47 +0300

    Дизасм. различная сборка и вычисления

--- eduSem_4/machineDependentProgrammingLanguages/laboratoryWorks/LR_9/programmSolution/src/32_bit_measures.c
+++ eduSem_4/machineDependentProgrammingLanguages/laboratoryWorks/LR_9/programmSolution/src/32_bit_measures.c
@@ -24,6 +24,26 @@ void get_32_bit_sum_asm(float a, float b, size_t count)
 }
 #endif
 
+// 
+#ifdef ASM
+void get_32_bit_sum_asm_proc(float a, float b, size_t count)
+{
+    float c;
+
+    for (size_t i = 0; i < count; ++i)
+    {
+        asm(
+            "mov eax, %1\n"
+            "add eax, %2\n"
+            "mov %0, eax\n" : "=m"(c)
+            : "m"(a), "m"(b)
+            );
+    }
+
+    printf("Сложение процессором: %lf + %lf = %lf\n", a, b, c);
+}
+#endif
+
 void get_32_bit_prod(float a, float b, size_t count)
 {
     float c;
@@ -50,7 +70,7 @@ void out_32_bit_dimension()
 {
     printf("float type size: %zu bites\n", sizeof(float) * CHAR_BIT);
 
-    float a = 2e43, b = 11e53;
+    float a = 1000.9797, b = 100000.456789;
 
     clock_t begin = clock();
     get_32_bit_sum(a, b, repeats);
@@ -58,13 +78,22 @@ void out_32_bit_dimension()
 
     printf(SUM_FMT, (double)(end - begin) / CLOCKS_PER_SEC / repeats);
 
+    // Вычисление на процессоре
     #ifdef ASM
     begin = clock();
-    get_32_bit_sum_asm(a, b, repeats);
+    get_32_bit_sum_asm_proc(a, b, repeats);
     end = clock();
 
     printf(ASM_SUM_FMT, (double)(end - begin) / CLOCKS_PER_SEC / repeats);
     #endif
+
+    // #ifdef ASM
+    // begin = clock();
+    // get_32_bit_sum_asm(a, b, repeats);
+    // end = clock();
+
+    // printf(ASM_SUM_FMT, (double)(end - begin) / CLOCKS_PER_SEC / repeats);
+    // #endif
 
     begin = clock();
     get_32_bit_prod(a, b, repeats);
