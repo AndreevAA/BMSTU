65775af375bf764b87bceb831f92ce887d3de2f0
AndreevAA <58621780+AndreevAA@users.noreply.github.com>
2021-09-15 10:04:33 +0300

    Added progress_1 of 8th of September lab. work

---
+++ eduSem_5/EVM/LR_1/progress_1/user/Aptana Studio 3 Workspace/.metadata/.plugins/com.aptana.ruby.core/724921849/4/constants.rb
@@ -0,0 +1,13 @@
+ARGV = []
+TRUE = true
+FALSE = false
+STDOUT = IO.new
+STDIN = IO.new
+STDERR = IO.new
+ENV = {}
+NIL = nil
+RUBY_PATCHLEVEL = 247
+RUBY_COPYRIGHT = 'ruby - Copyright (C) 1993-2013 Yukihiro Matsumoto'
+TOPLEVEL_BINDING = Binding.new
+RUBY_VERSION = '2.0.0'
+RUBY_PLATFORM = 'i386-mingw32'
