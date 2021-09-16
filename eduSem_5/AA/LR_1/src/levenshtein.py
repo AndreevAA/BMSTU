76c076a4c76e798bfc3fc8fff8c14f201aa3d6b5
Andreev Alexander <andreev.work.mail@gmail.com>
2021-09-16 11:46:55 +0300

    added menu

--- eduSem_5/AA/LR_1/src/levenshtein.py
+++ eduSem_5/AA/LR_1/src/levenshtein.py
@@ -77,3 +77,41 @@ class LevenshteinRecursiveWithCache(Levenshtein):
 
         return get_distance()
 
+
+# Объект вычисления Дамерау_Левенштейна
+class DamerauLevenshtein(Levenshtein):
+    # Используемые блины строк
+    _first_string_length = None
+    _second_string_length = None
+
+    def __init__(self, first_string, second_string):
+        super().__init__(first_string, second_string)
+
+        self._first_string_length = len(self._first_string)
+        self._second_string_length = len(self._second_string)
+
+    # Получение расстояния мужду двумя строками
+    def get_distance(self):
+        d = {}
+
+        for i in range(-1, self._first_string_length + 1):
+            d[(i, -1)] = i + 1
+        for j in range(-1, self._second_string_length + 1):
+            d[(-1, j)] = j + 1
+
+        for i in range(self._first_string_length):
+            for j in range(self._second_string_length):
+                if self._first_string[i] == self._second_string[j]:
+                    cost = 0
+                else:
+                    cost = 1
+                d[(i, j)] = min(
+                    d[(i - 1, j)] + 1,  # deletion
+                    d[(i, j - 1)] + 1,  # insertion
+                    d[(i - 1, j - 1)] + cost,  # substitution
+                )
+                if i and j and self._first_string[i] == self._second_string[j - 1] and \
+                        self._first_string[i - 1] == self._second_string[j]:
+                    d[(i, j)] = min(d[(i, j)], d[i - 2, j - 2] + cost)  # transposition
+
+        return d[self._first_string_length - 1, self._second_string_length - 1]
--- eduSem_5/AA/LR_1/src/main.py
+++ eduSem_5/AA/LR_1/src/main.py
@@ -8,10 +8,39 @@ import levenshtein
 
 # Основная функция программы
 def main():
-    first_string = input("Input first_string: ")
-    second_string = input("Input second_string: ")
 
-    print(levenshtein.LevenshteinRecursiveWithCache(first_string, second_string).get_distance())
+    input_string = ""
+    while str(input_string) != "-1":
+
+        print("Введите строки:")
+        first_string = input("\tfirst_string = ")
+        second_string = input("\tsecond_string = ")
+
+        print("\nВыберите алгоритм:")
+        print("\t1  - Левенштейн рекурсивный без кэша")
+        print("\t2  - Левенштейн рекурсивный с кэшем")
+        print("\t3  - Дамерау-Левенштейн рекурсивный")
+        print("\t-1 - Выход\n")
+
+        algorithm = input("Выбранный алгоритм = ")
+
+        objLev = None
+
+        if algorithm == "1":
+            objLev = levenshtein.LevenshteinRecursiveWithoutCache(first_string, second_string)
+        elif algorithm == "2":
+            objLev = levenshtein.LevenshteinRecursiveWithCache(first_string, second_string)
+        elif algorithm == "3":
+            objLev = levenshtein.DamerauLevenshtein(first_string, second_string)
+        elif algorithm == "-1":
+            exit()
+
+        if algorithm == "1" or algorithm == "2" or algorithm == "3":
+            print("\nРезультат: ")
+            print("\tРасстояние =",  objLev.get_distance())
+            print()
+            print("--")
+            print()
 
 
 if __name__ == '__main__':
