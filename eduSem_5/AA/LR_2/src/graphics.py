e6845e8fd6deba2faad089437813b77e6729c8d3
Andreev Alexander <andreev.work.mail@gmail.com>
2021-09-24 00:36:09 +0300

    local changed of graphics

--- eduSem_5/AA/LR_2/src/graphics.py
+++ eduSem_5/AA/LR_2/src/graphics.py
@@ -1,18 +1,48 @@
 import config
 import matplotlib.pyplot as plt
 import numpy as np
+import graphic
+
 
 class Graphics:
-    def __init__(self):
+
+    # Графики
+    _first_graphic = None
+    _second_graphic = None
+    _third_graphic = None
+
+    # Данные о рисунке
+    _title = None
+    _ylabel = None
+
+    # Инициализация данных
+    def __init__(self, first_grahic, second_graphic, third_graphic, title, ylabel):
+
+        self._first_graphic = first_grahic
+        self._second_graphic = second_graphic
+        self._third_graphic = third_graphic
+
+        self._title = title
+        self._ylabel = ylabel
+
+    # Визуализация данных графиков
+    def visualize(self):
+
         rng = np.arange(50)
         rnd = np.random.randint(0, 10, size=(3, rng.size))
+        print(rnd)
         yrs = 1950 + rng
 
         fig, ax = plt.subplots(figsize=(5, 3))
-        ax.stackplot(yrs, rng + rnd, labels=['Eastasia', 'Eurasia', 'Oceania'])
-        ax.set_title('Combined debt growth over time')
+
+        ax.stackplot(yrs, rng + rnd, labels=[self._first_graphic.get_name(),
+                                             self._second_graphic.get_name(),
+                                             self._third_graphic.get_name()])
+
+        ax.set_title(self._title)
         ax.legend(loc='upper left')
-        ax.set_ylabel('Total debt')
+        ax.set_ylabel(self._ylabel)
+
         ax.set_xlim(xmin=yrs[0], xmax=yrs[-1])
         fig.tight_layout()
 
