ce98ba30472e599ff060981d89acfb12ee12a7c5
Andreev Alexander <andreev.work.mail@gmail.com>
2021-09-30 09:58:53 +0300

    Updated report

---
+++ eduSem_5/AA/LR_2/main.py
@@ -0,0 +1,15 @@
+'''
+    DB, LR_2, UI7-54B
+    created by Andreev Alexander
+    25 SQL requests to DB
+'''
+
+def main(name):
+
+    # SELECT WITH COMPARE
+
+    return
+
+# Start point
+if __name__ == '__main__':
+    main('PyCharm')
