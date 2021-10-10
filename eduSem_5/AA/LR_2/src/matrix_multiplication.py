91efd337f9d44b734cb29d5daf431819a8ec7ab5
Andreev Alexander <andreev.work.mail@gmail.com>
2021-10-10 20:50:37 +0300

    update classic copersmith

--- eduSem_5/AA/LR_2/src/matrix_multiplication.py
+++ eduSem_5/AA/LR_2/src/matrix_multiplication.py
@@ -66,10 +66,68 @@ class CoppersmittWinogradMultiplication(MatrixMultiplication):
         result_matrix = self._generate_result_matrix()
 
         _mul_h, _mul_v = [0 for _ in range(self._first_matrix.get_size_of_matrix())], \
-                       [0 for _ in range(self._second_matrix.get_size_of_matrix())]
+                         [0 for _ in range(self._second_matrix.get_size_of_matrix())]
 
         for check_row in range(self._first_matrix.get_size_of_matrix()):
+            for check_column in range(self._first_matrix.get_size_of_matrix() // 2):
+                _mul_h[check_row] += self._first_matrix.get_matrix_value()[check_row][2 * check_column] *\
+                                     self._first_matrix.get_matrix_value()[check_row][2 * check_column + 1]
 
+        for check_row in range(self._second_matrix.get_size_of_matrix()):
+            for check_column in range(self._second_matrix.get_size_of_matrix() // 2):
+                _mul_v[check_row] += self._second_matrix.get_matrix_value()[check_row][2 * check_column] * \
+                                     self._second_matrix.get_matrix_value()[check_row][2 * check_column + 1]
+
+        for check_row in range(self._first_matrix.get_size_of_matrix()):
+            for check_column in range(self._first_matrix.get_size_of_matrix()):
+                result_matrix.update_matrix_value(check_row, check_column,
+                                                  - _mul_h[check_row] - _mul_v[check_column]
+                                                  )
+
+                for k in range(self._first_matrix.get_size_of_matrix() // 2):
+                    result_matrix.update_matrix_value(check_row, check_column,
+                                                      result_matrix.get_matrix_value(check_row, check_column) +
+                                                      self._first_matrix.get_matrix_value()[check_row][2 * k] +
+                                                      self._second_matrix.get_matrix_value[2 * k + 1][
+                                                           check_column]) * (
+                                                              self._first_matrix.get_matrix_value[check_row][
+                                                                  2 * k + 1] +
+                                                              self._second_matrix.get_matrix_value[2 * k][
+                                                                  check_column]
+                                                      )
+
+        if self._second_matrix.get_size_of_matrix() % 2:
+            for check_row in range(self._first_matrix.get_size_of_matrix()):
+                for check_column in range(self._first_matrix.get_size_of_matrix()):
+                    result_matrix.update_matrix_value(check_row, check_column,
+                                                      result_matrix.get_matrix_value(check_row, check_column) +
+                                                      self._first_matrix.get_matrix_value[check_row][self._first_matrix.get_size_of_matrix() - 1] *
+                                                      self._second_matrix[self._second_matrix.get_size_of_matrix() - 1][check_column]
+                                                      )
+
+        return result_matrix
+
+
+# Объект умножения матриц оптимизированный Копперсмитта-Винограда
+class CoppersmittWinogradOptimizedMultiplication(MatrixMultiplication):
+
+    def get_multiplied_matrix(self):
+        self._result_matrix = self._multiply()
+        return self._result_matrix
+
+    def _multiply(self):
+
+        # Необходимые для вычислений данные
+        _first_matrix_size, _second_matrix_size = self._first_matrix.get_size_of_matrix(), \
+                                                  self._second_matrix.get_size_of_matrix()
+
+        # Виртуальная результирующая матрица
+        result_matrix = self._generate_result_matrix()
+
+        _mul_h, _mul_v = [0 for _ in range(self._first_matrix.get_size_of_matrix())], \
+                         [0 for _ in range(self._second_matrix.get_size_of_matrix())]
+
+        for check_row in range(self._first_matrix.get_size_of_matrix()):
             _mul_h[check_row] = sum(
                 self._first_matrix.get_matrix_value()[check_row][2 * check_column] *
                 self._first_matrix.get_matrix_value()[check_row][2 * check_column + 1]
@@ -89,14 +147,12 @@ class CoppersmittWinogradMultiplication(MatrixMultiplication):
                                                       (self._first_matrix.get_matrix_value()[check_row][2 * k] +
                                                        self._second_matrix.get_matrix_value[2 * k + 1][
                                                            check_column]) * (
-                                                                  self._first_matrix.get_matrix_value[check_row][
-                                                                      2 * k + 1] +
-                                                                  self._second_matrix.get_matrix_value[2 * k][
-                                                                      check_column])
+                                                              self._first_matrix.get_matrix_value[check_row][
+                                                                  2 * k + 1] +
+                                                              self._second_matrix.get_matrix_value[2 * k][
+                                                                  check_column])
                                                       for k in range(self._first_matrix.get_size_of_matrix() // 2)) \
                                                   - _mul_h[check_row] - _mul_v[check_column]
                                                   )
 
         return result_matrix
-
-        
