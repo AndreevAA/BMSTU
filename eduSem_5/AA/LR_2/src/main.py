5ae8dcf02b04ae056f86a43d52374d50d59071e6
Andreev Alexander <andreev.work.mail@gmail.com>
2021-10-10 23:26:56 +0300

    UPDATE

--- eduSem_5/AA/LR_2/src/main.py
+++ eduSem_5/AA/LR_2/src/main.py
@@ -1,8 +1,53 @@
 import __config__
-
+import matrix
 
 # Основной блок программы
+import matrix_multiplication
+
+
 def main():
+
+    # mt_1 = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]]
+    # mt_2 = [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 4, 5, 6, 7], [4, 5, 6, 7, 8]]
+    #
+    # print("\nМатрица №1:")
+    # m_output(mt_1)
+    # print("\nМатрица №2:")
+    # m_output(mt_2)
+    #
+    # print("\nСтандартный алгоритм умножения матриц:")
+    # m_output(standard_alg(mt_1, mt_2))
+    #
+    # print("\nАлгоритм Винограда:")
+    # m_output(Winograd_alg(mt_1, mt_2))
+    #
+    # print("\nОптимизированный алгоритм Винограда:")
+    # m_output(Winograd_alg_improved(mt_1, mt_2))
+
+    first_matrix = matrix.Matrix(3,
+                                 [[1, 2, 3],
+                                  [4, 5, 6],
+                                  [7, 8, 9]]
+                                 )
+
+    second_matrix = matrix.Matrix(3,
+                                 [[1, 2, 3],
+                                  [4, 5, 6],
+                                  [7, 8, 9]]
+                                 )
+
+    first_second_classic = matrix_multiplication.ClassicalMultiplication(first_matrix, second_matrix)
+    first_second_CM = matrix_multiplication.CoppersmittWinogradMultiplication(first_matrix, second_matrix)
+    first_second_CM_optimized = matrix_multiplication.CoppersmittWinogradOptimizedMultiplication(first_matrix, second_matrix)
+
+    first_second_classic.multiply()
+    first_second_CM.multiply()
+    first_second_CM_optimized.multiply()
+
+    print(first_second_classic.get_result_processor_time())
+    print(first_second_CM.get_result_processor_time())
+    print(first_second_CM_optimized.get_result_processor_time())
+
     return __config__.SUCCESS_STATUS
 
 
--- eduSem_5/AA/LR_2/src/matrix_multiplication.py
+++ eduSem_5/AA/LR_2/src/matrix_multiplication.py
@@ -7,7 +7,6 @@ import matrix, analitics
 
 # Базовый объект Умножения матрицы
 class MatrixMultiplication:
-
     # Приватные рабочие поля
     _first_matrix = None
     _second_matrix = None
@@ -18,7 +17,6 @@ class MatrixMultiplication:
 
     # Создание объекта матрицы
     def __init__(self, first_matrix, second_matrix):
-
         # Установка введенных матриц
         self._first_matrix = first_matrix
         self._second_matrix = second_matrix
@@ -54,14 +52,14 @@ class MatrixMultiplication:
         return self._processor_time.set_end_processor_time()
 
     # Получение результата вычисления процессорного времени
-    def _get_result_processor_time(self):
+    def get_result_processor_time(self):
         return self._processor_time.get_result()
 
 
 # Объект классического умножения матрицы
 class ClassicalMultiplication(MatrixMultiplication):
 
-    def _multiply(self):
+    def multiply(self):
 
         # Установка отсечки времени
         self._set_start_processor_time()
@@ -87,7 +85,7 @@ class ClassicalMultiplication(MatrixMultiplication):
 # Объект умножения матриц Копперсмитта-Винограда
 class CoppersmittWinogradMultiplication(MatrixMultiplication):
 
-    def _multiply(self):
+    def multiply(self):
 
         # Установка отсечки времени
         self._set_start_processor_time()
@@ -104,13 +102,13 @@ class CoppersmittWinogradMultiplication(MatrixMultiplication):
 
         for check_row in range(self._first_matrix.get_size_of_matrix()):
             for check_column in range(self._first_matrix.get_size_of_matrix() // 2):
-                _mul_h[check_row] += self._first_matrix.get_matrix_value()[check_row][2 * check_column] *\
-                                     self._first_matrix.get_matrix_value()[check_row][2 * check_column + 1]
+                _mul_h[check_row] += self._first_matrix.get_matrix()[check_row][2 * check_column] * \
+                                     self._first_matrix.get_matrix()[check_row][2 * check_column + 1]
 
         for check_row in range(self._second_matrix.get_size_of_matrix()):
             for check_column in range(self._second_matrix.get_size_of_matrix() // 2):
-                _mul_v[check_row] += self._second_matrix.get_matrix_value()[check_row][2 * check_column] * \
-                                     self._second_matrix.get_matrix_value()[check_row][2 * check_column + 1]
+                _mul_v[check_row] += self._second_matrix.get_matrix()[check_row][2 * check_column] * \
+                                     self._second_matrix.get_matrix()[check_row][2 * check_column + 1]
 
         for check_row in range(self._first_matrix.get_size_of_matrix()):
             for check_column in range(self._first_matrix.get_size_of_matrix()):
@@ -121,22 +119,25 @@ class CoppersmittWinogradMultiplication(MatrixMultiplication):
                 for k in range(self._first_matrix.get_size_of_matrix() // 2):
                     result_matrix.update_matrix_value(check_row, check_column,
                                                       result_matrix.get_matrix_value(check_row, check_column) +
-                                                      self._first_matrix.get_matrix_value()[check_row][2 * k] +
-                                                      self._second_matrix.get_matrix_value[2 * k + 1][
-                                                           check_column]) * (
-                                                              self._first_matrix.get_matrix_value[check_row][
-                                                                  2 * k + 1] +
-                                                              self._second_matrix.get_matrix_value[2 * k][
-                                                                  check_column]
-                                                      )
+                                                      self._first_matrix.get_matrix()[check_row][2 * k] +
+                                                      self._second_matrix.get_matrix()[2 * k + 1][
+                                                          check_column] * \
+                                                        (
+                                                                self._first_matrix.get_matrix()[check_row][
+                                                                    2 * k + 1] +
+                                                                self._second_matrix.get_matrix()[2 * k][
+                                                                    check_column]
+                                                        ))
 
         if self._second_matrix.get_size_of_matrix() % 2:
             for check_row in range(self._first_matrix.get_size_of_matrix()):
                 for check_column in range(self._first_matrix.get_size_of_matrix()):
                     result_matrix.update_matrix_value(check_row, check_column,
                                                       result_matrix.get_matrix_value(check_row, check_column) +
-                                                      self._first_matrix.get_matrix_value[check_row][self._first_matrix.get_size_of_matrix() - 1] *
-                                                      self._second_matrix[self._second_matrix.get_size_of_matrix() - 1][check_column]
+                                                      self._first_matrix.get_matrix()[check_row][
+                                                          self._first_matrix.get_size_of_matrix() - 1] *
+                                                      self._second_matrix.get_matrix()[
+                                                          self._second_matrix.get_size_of_matrix() - 1][check_column]
                                                       )
 
         # Установка отсечки времени
@@ -148,7 +149,7 @@ class CoppersmittWinogradMultiplication(MatrixMultiplication):
 # Объект умножения матриц оптимизированный Копперсмитта-Винограда
 class CoppersmittWinogradOptimizedMultiplication(MatrixMultiplication):
 
-    def _multiply(self):
+    def multiply(self):
 
         # Установка отсечки времени
         self._set_start_processor_time()
@@ -165,14 +166,14 @@ class CoppersmittWinogradOptimizedMultiplication(MatrixMultiplication):
 
         for check_row in range(self._first_matrix.get_size_of_matrix()):
             _mul_h[check_row] = sum(
-                self._first_matrix.get_matrix_value()[check_row][2 * check_column] *
-                self._first_matrix.get_matrix_value()[check_row][2 * check_column + 1]
+                self._first_matrix.get_matrix()[check_row][2 * check_column] *
+                self._first_matrix.get_matrix()[check_row][2 * check_column + 1]
                 for check_column in range(self._first_matrix.get_size_of_matrix() // 2)
             )
 
             _mul_v[check_row] = sum(
-                self._second_matrix.get_matrix_value()[check_row][2 * check_column] *
-                self._second_matrix.get_matrix_value()[check_row][2 * check_column + 1]
+                self._second_matrix.get_matrix()[check_row][2 * check_column] *
+                self._second_matrix.get_matrix()[check_row][2 * check_column + 1]
                 for check_column in range(self._second_matrix.get_size_of_matrix() // 2)
             )
 
@@ -180,12 +181,12 @@ class CoppersmittWinogradOptimizedMultiplication(MatrixMultiplication):
             for check_column in range(self._first_matrix.get_size_of_matrix()):
                 result_matrix.update_matrix_value(check_row, check_column,
                                                   sum(
-                                                      (self._first_matrix.get_matrix_value()[check_row][2 * k] +
-                                                       self._second_matrix.get_matrix_value[2 * k + 1][
+                                                      (self._first_matrix.get_matrix()[check_row][2 * k] +
+                                                       self._second_matrix.get_matrix()[2 * k + 1][
                                                            check_column]) * (
-                                                              self._first_matrix.get_matrix_value[check_row][
+                                                              self._first_matrix.get_matrix()[check_row][
                                                                   2 * k + 1] +
-                                                              self._second_matrix.get_matrix_value[2 * k][
+                                                              self._second_matrix.get_matrix()[2 * k][
                                                                   check_column])
                                                       for k in range(self._first_matrix.get_size_of_matrix() // 2)) \
                                                   - _mul_h[check_row] - _mul_v[check_column]
