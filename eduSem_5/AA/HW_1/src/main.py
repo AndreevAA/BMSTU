76930937f7d7b4c5eeef3d318b7a820875ee863c
AndreevAA <58621780+AndreevAA@users.noreply.github.com>
2021-10-26 23:54:28 +0300

    finished report and added graphs

--- eduSem_5/AA/HW_1/src/main.py
+++ eduSem_5/AA/HW_1/src/main.py
@@ -13,23 +13,19 @@ def main():
                  list(map(float, input("X: ").split())), \
                  list(map(float, input("X: ").split())), \
                  int(input("Введите степень аппроксимации: "))
-
     N = len(x)
-
     A = zeros((n + 1, n + 1))
     B = zeros((n + 1, 1))
-
     for j in range(n + 1):
         for k in range(n + 1):
             for i in range(N):
                 A[j, k] += f(x[i], k + j) * p[i]
         for l in range(N):
             B[j] += y[l] * f(x[l], j) * p[l]
-
     det = linalg.det(A)
-
+    nparray_to_list(A)
+    nparray_to_list(b)
     print("Определитель:", det)
-
     return config.SUCCESS_STATUS
 
 
