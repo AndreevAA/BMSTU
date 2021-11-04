f064f7eb579af3f20b3f10a6abec2bd5e5a86651
Andreev Alexander <andreev.work.mail@gmail.com>
2021-11-04 13:05:21 +0300

    full 4

--- eduSem_5/DB/LR_3/tasl_5.sql
+++ eduSem_5/DB/LR_3/tasl_5.sql
@@ -1,6 +1,6 @@
--- А. Четыре функции
+-- B. Четыре хранимые процедуры
 
--- 4) Рекурсивная функция или функция с рекурсивным ОТВ
+-- 1) Хранимую процедуру без параметров или с параметрами
 -- Рекурсивный вывод информации о танках по их id
 create or replace function recursive_hull_information(int, int)
 returns setof military_vehicles.public.hull as
