e4326d0c49e1cde4af4a8cc29d0e8df7b6e30617
Andreev Alexander <andreev.work.mail@gmail.com>
2021-11-04 14:08:31 +0300

    project initialized

---
+++ eduSem_5/DB/LR_4/main.py
@@ -0,0 +1,33 @@
+import db, config, requests
+
+
+# Основной блок
+def main():
+    # Создание объект БД
+    operation_db = db.DB(config.DB_NAME, config.DB_HOST, config.DB_PORT)
+
+    # Подключение к БД
+    operation_db.create_connection(config.DB_USER, config.DB_PASSWORD)
+
+    # Запрос к БД
+    request = requests.Request(operation_db)
+
+    for _task_number in range(1, 11):
+        print("task_" + str(_task_number) + ": ")
+        if 4 >= _task_number:
+            try:
+                get_by_task_query = request.get_by_task_number(_task_number).fetchall()
+                print(get_by_task_query)
+            except Exception:
+                print("Som errors while SQL...")
+        elif 4 < _task_number:
+            try:
+                request.get_by_task_number(_task_number)
+            except Exception:
+                print("Som errors while SQL...")
+        print("---")
+
+
+# Точка входа
+if __name__ == '__main__':
+    main()
---
+++ eduSem_5/DB/LR_4/requests.py
@@ -0,0 +1,28 @@
+import config
+
+
+# Объект обертки набора запросов
+class Request:
+    # Приватные данные
+    _db = None
+
+    # Инициализация объекта
+    def __init__(self, db_source):
+        # Установка значений в объект
+        self._db = db_source
+
+    # Получение распакованного SQL
+    def _fetch_sql(self, query):
+        return self._db.execute_query(query)
+        # try:
+        #     return self._db.execute_query(query).fetchall()
+        # except:
+        #     return config.empty_sql
+
+    # Получение по номеру таска
+    def get_by_task_number(self, task_number):
+        query = str(open("task_" + str(task_number) + ".sql").read()) +\
+                str(open("task_" + str(task_number) + "_exec.sql").read())
+
+        # Возврат полученного SQL
+        return self._fetch_sql(query)
