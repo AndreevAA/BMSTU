b891d5b30ab6556a761e65c54efc0f693554775e
Andreev Alexander <58621780+AndreevAA@users.noreply.github.com>
2021-12-26 22:54:04 +0300

    Update script.sql

--- eduSem_5/DB/ADD_2/script.sql
+++ eduSem_5/DB/ADD_2/script.sql
@@ -1,37 +1,36 @@
--- Создать таблицы:
--- • Table1{id: integer, var1: string, valid_from_dttm: date, valid_to_dttm: date}
--- • Table2{id: integer, var2: string, valid_from_dttm: date, valid_to_dttm: date}
--- Версионность в таблицах непрерывная, разрывов нет (если valid_to_dttm = '2018-09-05', то для следующей строки соответствующего ID valid_from_dttm = '2018-09-06', т.е. на день больше). Для каждого ID дата начала версионности и дата конца версионности в Table1 и Table2 совпадают.
+drop table if exists romashka;
 
-create table if not exists Table1(
-	id integer,
-	var1 text,
-	valid_from_dttm date,
-	valid_to_dttm date
+create table romashka (
+	department varchar not null,
+	FIO varchar not null,
+	Date date not null ,
+	Status varchar not null
 );
 
-create table if not exists Table2(
-	id integer,
-	var2 text,
-	valid_from_dttm date,
-	valid_to_dttm date
-);
+insert into romashka(department, FIO, Date, Status) values
+('ИТ','Иванов Иван Иванович','2020-01-15','Больничный'),
+('ИТ','Иванов Иван Иванович','2020-01-16','На работе'),
+('ИТ','Иванов Иван Иванович','2020-01-17','Оплачиваемый отпуск'),
+('ИТ','Иванов Иван Иванович','2020-01-18','На работе'),
+('ИТ','Иванов Иван Иванович','2020-01-19','Оплачиваемый отпуск'),
+('ИТ','Иванов Иван Иванович','2020-01-20','Оплачиваемый отпуск'),
+('Бухгалтерия','Петрова Ирина Ивановна','2020-01-15','Оплачиваемый отпуск'),
+('Бухгалтерия','Петрова Ирина Ивановна','2020-01-16','На работе'),
+('Бухгалтерия','Петрова Ирина Ивановна','2020-01-17','На работе'),
+('Бухгалтерия','Петрова Ирина Ивановна','2020-01-18','На работе'),
+('Бухгалтерия','Петрова Ирина Ивановна','2020-01-19','Оплачиваемый отпуск'),
+('Бухгалтерия','Петрова Ирина Ивановна','2020-01-20','Оплачиваемый отпуск');
 
-insert into Table1 values
-(1, 'A', '2018-09-01', '2018-09-15'),
-(1, 'B', '2018-09-16', '5999-12-31');
+select * from romashka;
 
-insert into Table2 values
-(1, 'A', '2018-09-01', '2018-09-18'),
-(1, 'B', '2018-09-19', '5999-12-31');
-
-with time_union AS (
-	SELECT Table1.id, var1, var2, GREATEST(Table1.valid_from_dttm, Table2.valid_from_dttm) AS valid_from_dttm,
-	LEAST(Table1.valid_to_dttm, Table2.valid_to_dttm) AS valid_to_dttm
-	FROM Table1 JOIN Table2 ON Table1.id = table2.id
+with extra_table as (
+    select department, fio, date, status,
+           min(date) over (partition by department, fio, status) as min_date,
+           max(date) over (partition by department, fio, status) as max_date
+    from romashka
+    order by fio, max_date
 )
+select distinct  department, fio, min_date as DateFrom, max_date as DateTo, status
+from extra_table
+order by department desc, DateFrom;
 
-SELECT *
-FROM time_union
-WHERE valid_from_dttm <= valid_to_dttm
-ORDER BY valid_from_dttm;
