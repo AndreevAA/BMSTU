6c309395fd355b7dfd3307f338185ae9bc1286d8
AndreevAA <58621780+AndreevAA@users.noreply.github.com>
2022-02-16 22:22:33 +0300

    Update

--- eduSem_5/OS/LR_8(2)/main.c
+++ eduSem_5/OS/LR_8(2)/main.c
@@ -5,8 +5,41 @@
 #include <unistd.h>
 #include <errno.h>
 
-//путь	//глубина рекурсии
-static int dopath(const char *filename, int depth) {
+#define OK 0
+#define ERROR -1
+#define ERROR_STAT 1
+#define ERROR_OPEN_DIR 2
+
+#define START_DEPTH 0
+
+// Рекурсивный проход по пути, его вывод и возврат ошибки
+static int dopath(const char *filename, int recursion_depth);
+
+// Проверка наличия аргумента пути
+int is_dir_in_arg(int argc);
+
+int main(int argc, char *argv[]) 
+{
+	int error_status; 
+
+	// Параметр отсутствует
+	if (is_dir_in_arg(argc) == OK) 
+		error_status = dopath(argv[1], START_DEPTH);	// начиная с папки в параметре
+	else
+		error_status = dopath("./", 0); // начиная с текущей папки
+
+	return error_status;
+}
+
+int is_dir_in_arg(int argc)
+{
+	if (argc == 2)
+		return OK;
+	return ERROR;
+}
+
+static int dopath(const char *filename, int recursion_depth) 
+{
 	struct stat statbuf;
 	struct dirent *dirp;
 	DIR *dp;
@@ -14,7 +47,7 @@ static int dopath(const char *filename, int depth) {
 	//'.' - указатель на сам каталог
 	//'..' - указатель на родительский каталог
 	if (strcmp(filename, ".") == 0 || strcmp(filename, "..") == 0)
-		return 0;
+		return OK;
 
 	//Вызов lstat() идентичен stat(), но в случае, если filename является символьной ссылкой, то возвращается информация о самой ссылке, а не о файле, на который она указывает; возвращает информацию об указанном файле
 	if (lstat(filename, &statbuf) < 0) {
@@ -49,12 +82,12 @@ static int dopath(const char *filename, int depth) {
 				break;	
 		}		
 		printf("Ошибка вызова функции stat");
-		return -1;
+		return ERROR_STAT;
 
 	}
 	
 	//печать отступов
-	for (int i = 0; i < depth; ++i)
+	for (int i = 0; i < recursion_depth; ++i)
 		printf("       |");
 	
 
@@ -62,14 +95,14 @@ static int dopath(const char *filename, int depth) {
 	// s_isdir проверяет, является ли данный файл каталогом
 	if (S_ISDIR(statbuf.st_mode) == 0) {
 		printf("%s\n", filename);
-		return 0;
+		return OK;
 	}
 
 	/* каталог */
 	printf("%s\n", filename);
 	if ((dp = opendir(filename)) == NULL) {
 		printf("couldn't open directory '%s'\n", filename);
-		return 1;
+		return ERROR_OPEN_DIR;
 	}
 
 	// chdir() изменяет текущий рабочий каталог вызывающего процесса на каталог, указанный в path; PATH — переменная окружения, представляющая собой набор каталогов, в которых расположены исполняемые файлы; нужен для коротких имен
@@ -79,23 +112,13 @@ static int dopath(const char *filename, int depth) {
 	// для каждого элемента каталога
 	// Функция readdir() возвращает указатель на следующую запись каталога в структуре dirent, прочитанную из потока каталога. Каталог указан в dir. Функция возвращает NULL по достижении последней записи или если была обнаружена ошибка.
 	while ((dirp = readdir(dp)) != NULL) 
-		dopath(dirp -> d_name, depth+1);
+		dopath(dirp -> d_name, recursion_depth + 1);
 	chdir("..");
 
 	//закрывает поток каталога и освобождает ресурсы, выделенные ему. 
 	//Она возвращает 0 в случае успеха и -1 при наличии ошибки.
 	closedir(dp);
 
-	return 0;
-} 
-
-int main(int argc, char *argv[]) {
-	int ret; ///код возврата
-
-	if (argc != 2) // нет параметра
-		ret = dopath("./",0); // начиная с текущей папки
-	else
-		ret = dopath(argv[1],0);	// начиная с папки в параметре
-
-	return ret;
+	return OK;
 }
+
